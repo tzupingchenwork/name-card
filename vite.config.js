65fd1ea7114c7ca2a6425eb4ab04eb8a43e4fd48
andychentzupingwork <andychentzupingwork@gmail.com>
2022-11-24 00:31:37 +0800

    build: base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
 
 export default defineConfig({
-  base: './name-card/',
+  base: 'https://github.com/tzupingchenwork//name-card/',
   plugins: [
     vue(),
     eslintPlugin({
